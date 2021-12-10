ccc0b5c7b0cb6736041f9547d944a8bd6552fd40
Deep Majumder <deep.majumder2019@gmail.com>
2021-12-10 11:15:32 +0530

    Fixed up reset return type

--- src/bsp/raspberrypi/power.rs
+++ src/bsp/raspberrypi/power.rs
@@ -4,12 +4,14 @@ const PM_WDOG_MAGIC: u32 = 0x5A00_0000;
 const PM_RSTC_FULLRST: u32 = 0x0000_0020;
 
 #[allow(dead_code)]
-pub fn board_reset() {
+pub fn board_reset() -> ! {
     let mut r = unsafe { core::ptr::read(board_pm_rsts()) };
     r &= !0xFFFF_FAAA;
     unsafe {
         core::ptr::write_volatile(board_pm_rsts() as *mut _, PM_WDOG_MAGIC | r);
         core::ptr::write_volatile(board_pm_wdog() as *mut _, PM_WDOG_MAGIC | 10);
         core::ptr::write_volatile(board_pm_rstc() as *mut _, PM_WDOG_MAGIC | PM_RSTC_FULLRST);
-    }
+    };
+    // Should never be executed
+    loop {}
 }
