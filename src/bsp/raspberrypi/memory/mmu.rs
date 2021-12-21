cb1e3d883059180676c0289a7ea28477d3f6aaff
Deep Majumder <deep.majumder2019@gmail.com>
2021-12-21 22:54:33 +0530

    Remove remapped MMIO

--- src/bsp/raspberrypi/memory/mmu.rs
+++ src/bsp/raspberrypi/memory/mmu.rs
@@ -5,7 +5,7 @@ use crate::memory::mmu::*;
 
 pub type KernelAddrSpace = AddressSpace<{ memory_map::END_INCLUSIVE + 1 }>;
 
-const NUM_MEM_RANGES: usize = 3;
+const NUM_MEM_RANGES: usize = 2;
 
 // The virtual memory layout
 // The layout must contain only special ranges, ie, anything that is _not_ noermal cacheable DRAM
@@ -24,16 +24,6 @@ pub static LAYOUT: KernelVirtualLayout<NUM_MEM_RANGES> = KernelVirtualLayout::ne
             },
         },
         TranslationDescriptor {
-            name: "Remapped Device MMIO",
-            virtual_range: remapped_mmio_range_inclusive,
-            physical_range_translation: Translation::Offset(memory_map::mmio::START + 0x20_0000),
-            attribute_fields: AttributeFields {
-                mem_attributes: MemAttributes::Device,
-                acc_perms: AccessPermissions::ReadWrite,
-                execute_never: true,
-            },
-        },
-        TranslationDescriptor {
             name: "Device MMIO",
             virtual_range: mmio_range_inclusive,
             physical_range_translation: Translation::Identity,
@@ -48,11 +38,6 @@ pub static LAYOUT: KernelVirtualLayout<NUM_MEM_RANGES> = KernelVirtualLayout::ne
 
 fn code_range_inclusive() -> RangeInclusive<usize> {
     RangeInclusive::new(super::code_start(), super::code_end_exclusive() - 1)
-}
-
-fn remapped_mmio_range_inclusive() -> RangeInclusive<usize> {
-    // The last 64 KiB slot in the first 512 MiB
-    RangeInclusive::new(0x1FFF_0000, 0x1FFF_FFFF)
 }
 
 fn mmio_range_inclusive() -> RangeInclusive<usize> {
